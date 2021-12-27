443f44ec49835874fab45bde9508ca44cd3d6b27
SSKeerthivaasan <91375660+SSKeerthivaasan@users.noreply.github.com>
2021-12-27 10:00:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+name_of_the_student_array=[];
+
+function submit()
+{ 
+    var_display_student_array = [];
+
+    for (var j =1; j <= 4; j++)
+    {
+        var name_of_the_student = document.getElementById()
+    }
+}
